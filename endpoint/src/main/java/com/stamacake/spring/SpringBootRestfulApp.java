bec2ec3b9815722e27c220d1fc27eafcb4f42838
stamacake <ufigroman@gmail.com>
2020-07-13 15:20:23 +0300

    init

---
+++ endpoint/src/main/java/com/stamacake/spring/SpringBootRestfulApp.java
@@ -0,0 +1,12 @@
+package com.stamacake.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRestfulApp.class, args);
+    }
+}
---
+++ endpoint/src/main/java/com/stamacake/spring/dao/EmployeeDAO.java
@@ -0,0 +1,52 @@
+package com.stamacake.spring.dao;
+
+import com.stamacake.spring.model.Employee;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class EmployeeDAO {
+
+    private static final Map<String, Employee> empMap = new HashMap<String, Employee>();
+
+    static {
+        initEmps();
+    }
+
+    private static void initEmps() {
+        Employee emp1 = new Employee("1", "John", "Killer");
+        Employee emp2 = new Employee("2", "Wick", "Baba Yaga");
+        Employee emp3 = new Employee("3", "Continental", "Hitman");
+
+        empMap.put(emp1.getEmpNo(), emp1);
+        empMap.put(emp2.getEmpNo(), emp2);
+        empMap.put(emp3.getEmpNo(), emp3);
+    }
+
+    public Employee getEmployee(String empNo) {
+        return empMap.get(empNo);
+    }
+
+    public Employee addEmployee(Employee emp) {
+        empMap.put(emp.getEmpNo(), emp);
+        return emp;
+    }
+
+    public Employee updateEmployee(Employee emp) {
+        empMap.put(emp.getEmpNo(), emp);
+        return emp;
+    }
+
+    public void deleteEmployee(String empNo) {
+        empMap.remove(empNo);
+    }
+
+    public List<Employee> getAllEmployees() {
+        Collection<Employee> c = empMap.values();
+        List<Employee> list = new ArrayList<Employee>();
+        list.addAll(c);
+        return list;
+    }
+
+}
