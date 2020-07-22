26c73350e81386e39b3bc01f8358d5b7a6103d52
stamacake <ufigroman@gmail.com>
2020-07-23 01:01:54 +0300

    endpoint simple

--- endpoint/src/main/java/com/stamacake/spring/controller/MainRESTController.java
+++ endpoint/src/main/java/com/stamacake/spring/controller/MainRESTController.java
@@ -1,7 +1,6 @@
 package com.stamacake.spring.controller;
 
-import com.stamacake.spring.dao.EmployeeDAO;
-import com.stamacake.spring.model.Employee;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +12,6 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.List;
 
 @RestController
 public class MainRESTController {
@@ -21,10 +19,7 @@ public class MainRESTController {
     @Value("${URL_CONFIRM}")
     private String URL_CONFIRM;
 
-    @Autowired
-    private EmployeeDAO employeeDAO;
-
-    private static final Logger logger = LoggerFactory.getLogger(MainRESTController.class);;
+    private static final Logger logger = LoggerFactory.getLogger(MainRESTController.class);
 
     @Autowired
     RestTemplate restTemplate;
@@ -37,79 +32,19 @@ public class MainRESTController {
         return "Welcome";
     }
 
-    @RequestMapping(value = "/employees", //
-            method = RequestMethod.GET,
-            produces = { "application/json" })
+    @RequestMapping("/*")
+    public @ResponseBody String editRequest() {
 
-
-    public  @ResponseBody String getEmployees() {
-
-        logger.info("Request: "+ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
-        logger.info("From/to: "+((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
+        logger.info("Request: " + ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
+        logger.info("From/to: " + ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
                 .getRequest().getRemoteAddr());
-        List<Employee> list = employeeDAO.getAllEmployees();
 
-        HttpEntity<String> entity2;
-        entity2= new HttpEntity<String>(list.toString(), new HttpHeaders());
-        logger.info("ENTITY: "+entity2.getBody());
-
+        logger.info("TO:" + URL_CONFIRM);
         ResponseEntity<String> response = restTemplate.exchange(URL_CONFIRM,
-                HttpMethod.POST, entity2, String.class);
+                HttpMethod.GET, entity, String.class);
 
-        logger.info("BODY "+response.getBody());
+        logger.info("BODY " + response.getBody());
 
-        logger.info("Result: "+list.toString());
-
-
-
-        return response.getBody().toString();
+        return response.getBody();
     }
-
-    @RequestMapping(value = "/employee/{empNo}", //
-            method = RequestMethod.GET, //
-            produces = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseBody
-    public Employee getEmployee(@PathVariable("empNo") String empNo) {
-        logger.info("Request: "+ServletUriComponentsBuilder.fromCurrentRequest().toUriString());
-        logger.info("From/to: "+((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes())
-                .getRequest().getRemoteAddr());
-        logger.info("Result: "+employeeDAO.getEmployee(empNo).toString());
-        return employeeDAO.getEmployee(empNo);
-    }
-
-
-    @RequestMapping(value = "/employee", //
-            method = RequestMethod.POST, //
-            produces = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseBody
-    public Employee addEmployee(@RequestBody Employee emp) {
-
-        logger.info("Creating employee: " + emp.getEmpNo());
-        return employeeDAO.addEmployee(emp);
-    }
-
-
-    @RequestMapping(value = "/employee", //
-            method = RequestMethod.PUT, //
-            produces = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseBody
-    public Employee updateEmployee(@RequestBody Employee emp) {
-
-        logger.info("Creating employee: " + emp.getEmpNo());
-
-        return employeeDAO.updateEmployee(emp);
-    }
-
-
-    @RequestMapping(value = "/employee/{empNo}", //
-            method = RequestMethod.DELETE, //
-            produces = { MediaType.APPLICATION_JSON_VALUE })
-    @ResponseBody
-    public void deleteEmployee(@PathVariable("empNo") String empNo) {
-
-        logger.info("(Service Side) Deleting employee: " + empNo);
-
-        employeeDAO.deleteEmployee(empNo);
-    }
-
 }
--- endpointconfirm/src/main/java/com/stamacake/endpointconfirm/confirmController.java
+++ endpointconfirm/src/main/java/com/stamacake/endpointconfirm/confirmController.java
@@ -1,24 +1,22 @@
 package com.stamacake.endpointconfirm;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 
 
 @RestController
 public class confirmController {
 
+    private static final Logger logger = LoggerFactory.getLogger(confirmController.class);;
 
-
-    @RequestMapping("/confirm")
+    @RequestMapping("/*")
     public @ResponseBody String confirm(@RequestBody String httpEntity){
+        logger.info("HttpEntity: "+httpEntity);
         return "confirmed: "+httpEntity;
     }
 }
